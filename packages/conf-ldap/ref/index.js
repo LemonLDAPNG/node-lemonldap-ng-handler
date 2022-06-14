4c96d1b7b37464926fbb0d792f2f3e4d347000ae
Yadd <yadd@debian.org>
2022-06-14 06:38:25 +0200

    @LLNG/conf-ldap

---
+++ packages/conf-ldap/ref/index.js
@@ -0,0 +1,149 @@
+(function() {
+  /*
+   * LemonLDAP::NG LDAP configuration accessor for Node.js
+   *
+   * See README.md for license and copyright
+   */
+  var ldapConf;
+
+  ldapConf = class ldapConf {
+    constructor(args) {
+      var a, caData, caError, i, len, ref;
+      ref = ['ldapServer', 'ldapConfBase'];
+      // ldapServer ldapConfBase ldapBindDN ldapBindPassword
+      for (i = 0, len = ref.length; i < len; i++) {
+        a = ref[i];
+        if (!args[a]) {
+          throw `Missing ${a} argument`;
+        }
+      }
+      this.ldapServer = args.ldapServer.match(/^ldap/) ? args.ldapServer : `ldap://${args.ldapServer}`;
+      this.ldapCa = args.ldapServer.match(/^ldaps/) ? args.ldapCAFile || '' : '';
+      this.ldapConfBase = args.ldapConfBase;
+      this.dn = args.ldapBindDN;
+      this.pwd = args.ldapBindPassword;
+      this.objClass = args.ldapObjectClass || 'applicationProcess';
+      this.idAttr = args.ldapAttributeId || 'cn';
+      this.contentAttr = args.ldapAttributeContent || 'description';
+      this.base = args.ldapConfBase;
+      this.caConf = {};
+      if (this.ldapCa !== '') {
+        try {
+          caData = require('fs').readFileSync(this.ldapCa);
+          this.caConf = {
+            ca: [caData]
+          };
+        } catch (error) {
+          caError = error;
+        }
+      }
+      this.ldap = require('ldapjs').createClient({
+        tlsOptions: this.caConf,
+        url: this.ldapServer
+      });
+    }
+
+    available() {
+      var self;
+      self = this;
+      return new Promise(function(resolve, reject) {
+        return self.ldap.bind(self.dn, self.pwd, function(err) {
+          var data, opt;
+          if (err) {
+            return reject(`LDAP bind failed: ${err}`);
+          }
+          data = [];
+          opt = {
+            filter: `(objectClass=${self.objClass})`,
+            scope: 'sub',
+            attributes: [self.idAttr]
+          };
+          return self.ldap.search(self.ldapConfBase, opt, function(err, res) {
+            res.on('searchEntry', function(entry) {
+              return data.push(entry.object[self.idAttr].replace(/lmConf-/, ''));
+            });
+            res.on('error', function(err) {
+              return reject(`LDAP search failed: ${err}`);
+            });
+            return res.on('end', function(result) {
+              return resolve(data.sort(function(a, b) {
+                a = parseInt(a, 10);
+                b = parseInt(b, 10);
+                if (a === b) {
+                  return 0;
+                } else if (a < b) {
+                  return -1;
+                } else {
+                  return 1;
+                }
+              }));
+            });
+          });
+        });
+      });
+    }
+
+    lastCfg() {
+      var self;
+      self = this;
+      return new Promise(function(resolve, reject) {
+        return self.available().then(function(av) {
+          return resolve(av.pop());
+        }).catch(function(err) {
+          return reject(err);
+        });
+      });
+    }
+
+    load(cfgNum, fields) {
+      var q, self;
+      self = this;
+      return q = new Promise(function(resolve, reject) {
+        return self.ldap.bind(self.dn, self.pwd, function(err) {
+          var conf, data, opt;
+          if (err) {
+            return reject(`LDAP bind failed: ${err}`);
+          }
+          data = [];
+          conf = {};
+          opt = {
+            filter: `(objectClass=${self.objClass})`,
+            scope: 'sub'
+          };
+          return self.ldap.search(`${self.idAttr}=lmConf-${cfgNum},${self.base}`, opt, function(err, res) {
+            if (err) {
+              throw err;
+            }
+            res.on('searchEntry', function(entry) {
+              return data = entry.object[self.contentAttr];
+            });
+            res.on('error', function(err) {
+              return reject(`LDAP search failed: ${err}`);
+            });
+            return res.on('end', function(result) {
+              var $_, i, k, len, v;
+              for (i = 0, len = data.length; i < len; i++) {
+                $_ = data[i];
+                if (!$_.match(/^\{(.*?)\}(.*)/)) {
+                  reject(`Bad conf line: ${$_}`);
+                }
+                k = RegExp.$1;
+                v = RegExp.$2;
+                if ((v.match != null) && v.match(/^{/)) {
+                  conf[k] = JSON.parse(v);
+                } else {
+                  conf[k] = v;
+                }
+              }
+              return resolve(conf);
+            });
+          });
+        });
+      });
+    }
+
+  };
+
+  module.exports = ldapConf;
+
+}).call(this);
---
+++ packages/conf-ldap/src/index.ts
@@ -0,0 +1,146 @@
+export type LDAP_Args = {
+  ldapServer: string;
+  ldapConfBase: string;
+  ldapBindDN: string;
+  ldapBindPassword: string;
+  ldapObjectClass: string | undefined;
+  ldapAttributeId: string | undefined;
+  ldapAttributeContent: string | undefined;
+  ldapCAFile: string | undefined;
+};
+
+import ldapjs from 'ldapjs';
+import {LLNG_Conf} from '@LLNG/types';
+import type {SearchCallbackResponse, SearchEntry, SearchOptions} from 'ldapjs';
+import fs from 'fs';
+
+import type {Client as LDAPClient} from 'ldapjs';
+
+const requiredArgs = ['ldapServer','ldapConfBase','ldapBindDN','ldapBindPassword'];
+
+class LDAPConf {
+
+  /* properties */
+  ldapServer: string;
+  base: string;
+  dn: string;
+  pwd: string;
+  objClass: string;
+  idAttr: string;
+  contentAttr: string;
+  caConf: { ca: string[] } | undefined;
+  client: LDAPClient;
+  ldapCa: string;
+
+  constructor(args: LDAP_Args) {
+
+    // Check for required args
+    requiredArgs.forEach( (v) => {
+      // @ts-ignore
+      if(!args[v]) throw new Error(`argument ${v} is required`);
+    });
+    this.ldapServer = args.ldapServer.match(/^ldap/) ? args.ldapServer : `ldap://${args.ldapServer}`;
+    this.base = args.ldapConfBase;
+    this.dn = args.ldapBindDN;
+    this.pwd = args.ldapBindPassword;
+    this.objClass = args.ldapObjectClass || 'applicationProcess';
+    this.idAttr = args.ldapAttributeId || 'cn';
+    this.contentAttr = args.ldapAttributeContent || 'description';
+    this.ldapCa = args.ldapServer.match(/^ldaps/) ? args.ldapCAFile || '' : '';
+    if (this.ldapCa !== '') {
+      try {
+        this.caConf = {
+          ca: [fs.readFileSync(this.ldapCa).toString()]
+        };
+      } catch (error) {
+        throw new Error(`Unable to parse ${this.ldapCa}: ${error}`);
+      }
+    }
+    this.client = ldapjs.createClient({
+      tlsOptions: this.caConf,
+      url: this.ldapServer,
+    })
+  }
+
+  available() {
+    return new Promise<number[]>( (resolve, reject) => {
+      this.client.bind( this.dn, this.pwd, (err) => {
+        if (err) return reject(`LDAP bind failed: ${err}`);
+        const data: number[] = [];
+        const opt: SearchOptions = {
+          filter: `(objectClass=${this.objClass})`,
+          scope: 'sub',
+          attributes: [this.idAttr],
+        };
+        this.client.search(this.base, opt, (err: Error|null, res: SearchCallbackResponse) => {
+          res.on('searchEntry', (entry: SearchEntry) => {
+            let val = entry.object[this.idAttr];
+            if(typeof val === 'object') val = val[0];
+            data.push(parseInt(val.replace(/lmConf-/,''),10))
+          });
+          res.on('error', (err: Error) => {
+            return reject(`LDAP search failed: ${err}`);
+          });
+          res.on('end', () => {
+            resolve(data.sort());
+          });
+        });
+      });
+    });
+  }
+
+  lastCfg() {
+    return new Promise<number>( (resolve, reject) => {
+      this.available()
+        .then( (av: number[]) => {
+          if(av.length >0) {
+            // @ts-ignore
+            resolve(av.pop());
+          } else {
+            reject('No configuration available');
+          }
+        })
+        .catch( err => reject(err) );
+    });
+  }
+
+  load(cfgNum: number, fields: string[] = []) {
+    return new Promise<LLNG_Conf>( (resolve, reject) => {
+      this.client.bind(this.dn, this.pwd, err => {
+        if(err) return reject(`LDAP bind failed: ${err}`);
+        const opt: SearchOptions = {
+            filter: `(objectClass=${this.objClass})`,
+            scope: 'sub',
+        };
+        this.client.search(`${this.idAttr}=lmConf-${cfgNum},${this.base}`, opt, (err: Error|null, res: SearchCallbackResponse) => {
+          if(err) return reject(`LDAP search failed: ${err}`);
+          let data: string[];
+          // @ts-ignore: cfgNum initialized later
+          const conf: LLNG_Conf = {};
+          res.on('searchEntry', (entry: SearchEntry) => {
+            let tmp = entry.object[this.contentAttr];
+            data = typeof tmp === 'object' ? tmp : [tmp];
+          });
+          res.on('error', err => reject(`LDAP search failed: ${err}`));
+          res.on('end', (result) => {
+            data.forEach( (confLine: string) => {
+              if (!confLine.match(/^\{(.*?)\}(.*)/)) {
+                return reject(`Bad conf line: ${confLine}`);
+              }
+              let k = RegExp.$1;
+              let v = RegExp.$2;
+              if ((v.match !== null) && v.match(/^{/)) {
+                conf[k] = JSON.parse(v);
+              } else {
+                conf[k] = v;
+              }
+            });
+            return resolve(conf);
+          });
+        })
+      });
+    });
+  }
+}
+
+export default LDAPConf;
