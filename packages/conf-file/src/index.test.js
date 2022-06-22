7d8b3c64a9eb66957aeb69539d73f83667dbd82b
Yadd <yadd@debian.org>
2022-06-22 10:45:12 +0200

    improve test

--- packages/conf-file/src/index.test.js
+++ packages/conf-file/src/index.test.js
@@ -4,89 +4,85 @@ const FileConf = require('../')
 
 const fs = require('fs')
 const path = require('path')
-const rimraf = require('rimraf')
-const mkdirp = require('mkdirp')
 const dir = path.join(__dirname, 'conf')
 
 let fileConfs
 
 const clean = () => {
   try {
-    rimraf(dir, () => {})
+    fs.rmSync(dir, { recursive: true })
   } catch (e) {
-    console.error(e)
+    //console.error(e)
   }
 }
 
-beforeAll(async () => {
-  clean()
+describe('File configuration', () => {
+  beforeAll(async () => {
+    clean()
 
-  await mkdirp(dir)
-  fileConfs = new FileConf({
-    dirName: dir
+    fs.mkdirSync(dir)
+    fileConfs = new FileConf({
+      dirName: dir
+    })
   })
-})
+  afterAll(clean)
 
-afterAll(clean)
+  describe('when no configuration available', () => {
+    it('available() should return an empty array when no configuration available', () => {
+      expect(fileConfs.available()).resolves.toEqual([])
+    })
 
-test('store new conf', done => {
-  fileConfs
-    .store({
-      cfgNum: 1,
-      f1: 'field 1'
+    it('lastCfg() should throw', () => {
+      expect(fileConfs.lastCfg()).rejects.toEqual('No configuration available')
     })
-    .then(res => {
-      expect(res).toBeTrue
-      done()
-    })
-})
-
-test('read new conf', done => {
-  fileConfs
-    .load(1)
-    .then(res => {
-      expect(res.f1).toEqual('field 1')
-      done()
-    })
-    .catch(e => {
-      console.error(e)
-    })
-})
-
-test('store updated conf', done => {
-  fileConfs
-    .store({
-      cfgNum: 1,
-      f1: 'field 2'
-    })
-    .then(res => {
-      expect(res).toBeTrue
-      done()
-    })
-})
-
-test('read updated conf', done => {
-  fileConfs
-    .load(1)
-    .then(res => {
-      expect(res.f1).toEqual('field 2')
-      done()
-    })
-    .catch(e => {
-      console.error(e)
-    })
-})
-
-test('available', done => {
-  fileConfs.available().then(res => {
-    expect(res).toEqual([1])
-    done()
   })
-})
 
-test('lastCfg', done => {
-  fileConfs.lastCfg().then(res => {
-    expect(res).toEqual(1)
-    done()
+  describe('when configuration available', () => {
+    it('should store new conf', () => {
+      expect(
+        fileConfs.store({
+          cfgNum: 1,
+          f1: 'field 1'
+        })
+      ).resolves.toBeTrue
+    })
+
+    it('should find 1 available configuration', () => {
+      expect(fileConfs.available()).resolves.toEqual([1])
+    })
+
+    it('should return "1" as last available configuration', () => {
+      expect(fileConfs.lastCfg()).resolves.toEqual(1)
+    })
+    it('should read new conf', () => {
+      expect(fileConfs.load(1)).resolves.toEqual({ cfgNum: 1, f1: 'field 2' })
+    })
+
+    it('should store updated conf', () => {
+      expect(
+        fileConfs.store({
+          cfgNum: 1,
+          f1: 'field 2'
+        })
+      ).resolves.toBeTrue
+    })
+
+    it('should read updated conf', () => {
+      expect(fileConfs.load(1)).resolves.toEqual({ cfgNum: 1, f1: 'field 2' })
+    })
+
+    it('should find 1 available configuration', () => {
+      expect(fileConfs.available()).resolves.toEqual([1])
+    })
+
+    it('should return "1" as last available configuration', done => {
+      fileConfs.lastCfg().then(res => {
+        expect(res).toEqual(1)
+        done()
+      })
+      // Unusable here, else afterClean is launched before test ends
+      // See https://github.com/facebook/jest/issues/12957
+      //expect(fileConfs.lastCfg()).resolves.toEqual(1)
+    })
   })
 })
