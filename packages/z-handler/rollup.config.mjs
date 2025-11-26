aa0d0623d9bf92b0ce0ab40ddbf12557e897270c
Yadd <yadd@debian.org>
2025-11-26 19:46:24 +0100

    Fix build

--- packages/z-handler/rollup.config.mjs
+++ packages/z-handler/rollup.config.mjs
@@ -2,7 +2,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
-import copy from "rollup-plugin-copy";
 import cleaner from "rollup-plugin-cleaner";
 
 const alwaysExt = [
@@ -19,9 +18,6 @@ const alwaysExt = [
 const commonPlugins = [
   typescript(),
   commonjs(),
-  copy({
-    targets: [{ src: "../../CHANGELOG.md", dest: "./" }],
-  }),
 ];
 
 function configure(esm, external) {
